571d9b57fb5e31b8c3e36da5df5fb5ed89965a18
zryanf <80858407+zryanf@users.noreply.github.com>
2022-12-09 18:23:52 -0300

    refresh token parte 1

--- lib/repositories/login_repository.dart
+++ lib/repositories/login_repository.dart
@@ -3,10 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main(){
-  userLogin(cpf: '91769407057',userPassword: '12345');
-}
-
 String _listApi = dotenv.get("API_HOST", fallback: "");
 
 Future<int?> userLogin({cpf, userPassword}) async{
@@ -15,10 +11,11 @@ Future<int?> userLogin({cpf, userPassword}) async{
     var response = await http.post(url, body:{'userDocument':'$cpf', 'password':'$userPassword'});
     if(response.statusCode == 200){
       Map<String, dynamic> map = jsonDecode(response.body);
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      prefs.setString('access_token', jsonEncode(map['access_token'],),);
-      prefs.setString('refresh_token', jsonEncode(map['refresh_token']),);
-      prefs.setString('userCpf', jsonEncode(map['userCpf'],),);
+      accessToken(
+        accessToken: jsonEncode(map['access_token']),
+        refreshToken: jsonEncode(map['refresh_token']),
+        userCpf: jsonEncode(map['userCpf'],),
+      );
       return response.statusCode;
     }else{
       return response.statusCode;
@@ -26,4 +23,16 @@ Future<int?> userLogin({cpf, userPassword}) async{
   } catch(e){
     print(e);
   }
+}
+Future accessToken({accessToken, refreshToken,userCpf}) async{
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('access_token', accessToken);
+  prefs.setString('refresh_token', refreshToken);
+  prefs.setString('userCpf', userCpf);
+  SaveSession session = SaveSession(createAt: DateTime.now());
+}
+class SaveSession{
+  late DateTime createAt;
+
+  SaveSession({required this.createAt});
 }
